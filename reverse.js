dbc069234fe8afec93df9363b60ce51f5720fe11
Alejandra Aguilar <aleaguilar01@gmail.com>
2024-02-24 11:28:48 -0600

    Fixed bugs

--- reverse.js
+++ reverse.js
@@ -1,8 +1,11 @@
-var input = process.argv[2];
+const input = process.argv.slice(2);
 
-if (input)
+
+const reverse = function(original) {
+  return original.join().split('').reverse().join('');
+};
+
+
+if (input.length > 0) {
   console.log(reverse(input));
-
-function reverse(original) {
-  return orignal.split('').reverseList().join('');
 }
